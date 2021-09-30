6c4cb176f7a1182d718e58c03865e7541aae81c8
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-09-30 14:33:03 -0400

    Adjust on_member_join() to support DMing non-verified members

--- bot/cogs/Verification/verify.py
+++ bot/cogs/Verification/verify.py
@@ -52,6 +52,25 @@ class verify_commands(commands.Cog):
                                   "roles.")
             except:
                 return
+        else:
+            embed = discord.Embed(title="Welcome to Sykese's Discord Server!",
+                                  description="Make sure to read the below information to continue into the server.",
+                                  color=discord.Color.blue())
+            embed.add_field(name="Verification", value="We use a Hypixel based verification system. To verify, please "
+                            "use the command `/verify <minecraft username>`. If you're unsure of how to link your "
+                            "account, please view the gif pinned in {CHANNEL}.", inline=False)
+            embed.add_field(name="Rules & Information", value="Our server rules can be found in <#892118771257458738>. "
+                            "By joining our server, you agree to follow these rules. You can also find important "
+                            "information regarding the server and roles in this channel.", inline=False)
+            embed.add_field(name="Notification Roles", value="Once you verify, you'll get access to "
+                            "<#892208575273922581>. In this channel, you can choose selfroles that allow you to "
+                            "be notified for certain events, such as when Sykese uploads a video to his channel. "
+                            "These are completely optional, however we recommend that you enable them.", inline=False)
+            embed.set_footer(icon_url=member.guild.icon_url, text="Hope to see you around!")
+            try:
+                member.send(embed=embed)
+            except:
+                return
 
 def setup(bot):
     bot.add_cog(verify_commands(bot))
