2fd04ee2152a3249294b2b1a23ee8ab7ed32affc
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-10-02 15:52:19 -0400

    Add handler to remove unverified and add host check

--- bot/cogs/Verification/verify.py
+++ bot/cogs/Verification/verify.py
@@ -2,9 +2,11 @@ from discord.ext import commands
 from main import main_db
 from bot.utils.Misc.general import aiohttp_json, get_mojang_from_username
 from bot.utils.Checks.channel_checks import channel_restricted
-from variables import verified_role_id
+from variables import verified_role_id, unverified_role_id
 from datetime import datetime
 import discord
+import config
+
 users = main_db["users"]
 
 
@@ -18,7 +20,7 @@ class verify_commands(commands.Cog):
     async def verify(self, ctx, username):
         if users.find_one({"id": ctx.author.id}):
             await ctx.send("You're already verified to the account "
-                           f"`{users.find_one({'id': ctx.author.id}).get('uuid', 'ERROR')}`")
+                           f"`{users.find_one({'id' : ctx.author.id}).get('uuid', 'ERROR')}`")
         else:
             mojang = await get_mojang_from_username(username=username)
             player = await aiohttp_json(endpoint="player", attribute=mojang["id"])
@@ -43,33 +45,41 @@ class verify_commands(commands.Cog):
                 users.insert_one({"id": ctx.author.id, "uuid": mojang["id"],
                                   "verifiedAt": datetime.timestamp(datetime.now())})
                 await ctx.send("Successfully verified!")
+
+                try:
+                    await ctx.author.remove_roles(ctx.guild.get_role(unverified_role_id))
+                except:
+                    pass
             else:
                 await ctx.send("An unexpected error has occurred.")
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        if member.guild.id in [889697074491293736]:
+        if member.guild.id in [889697074491293736] and config.host == "master":
             if users.find_one({"id": member.id}) is not None:
                 await member.add_roles(member.guild.get_role(verified_role_id))
                 try:
                     await member.send("Welcome back! Because you previously verified, you have automatically received "
-                    "your roles.")
+                                      "your roles.")
                 except:
                     return
             else:
                 embed = discord.Embed(title="Welcome to Sykese's Discord Server!",
                                       description="Make sure to read the below information to continue into the server.",
                                       color=discord.Color.blue())
-                embed.add_field(name="Verification", value="We use a Hypixel based verification system. To verify, please "
-                                "use the command `/verify <minecraft username>`. If you're unsure of how to link your "
-                                "account, please view the gif pinned in {CHANNEL}.", inline=False)
-                embed.add_field(name="Rules & Information", value="Our server rules can be found in <#892118771257458738>. "
-                                "By joining our server, you agree to follow these rules. You can also find important "
-                                "information regarding the server and roles in this channel.", inline=False)
+                embed.add_field(name="Verification",
+                                value="We use a Hypixel based verification system. To verify, please "
+                                      "use the command `/verify <minecraft username>`. If you're unsure of how to link your "
+                                      "account, please view the gif pinned in <#893934059804319775>.", inline=False)
+                embed.add_field(name="Rules & Information",
+                                value="Our server rules can be found in <#892118771257458738>. "
+                                      "By joining our server, you agree to follow these rules. You can also find important "
+                                      "information regarding the server and roles in this channel.", inline=False)
                 embed.add_field(name="Notification Roles", value="Once you verify, you'll get access to "
                                 "<#892208575273922581>. In this channel, you can choose selfroles that allow you to "
                                 "be notified for certain events, such as when Sykese uploads a video to his channel. "
-                                "These are completely optional, however we recommend that you enable them.", inline=False)
+                                "These are completely optional, however we recommend that you enable them.",
+                                inline=False)
                 embed.set_footer(icon_url=member.guild.icon_url, text="Hope to see you around!")
                 try:
                     await member.send(embed=embed)
