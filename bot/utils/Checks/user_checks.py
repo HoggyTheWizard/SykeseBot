5c7781c8b570840580203a035e51458898b25433
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-10-03 14:05:05 -0400

    Add is_staff() decorator

--- bot/utils/Checks/user_checks.py
+++ bot/utils/Checks/user_checks.py
@@ -12,3 +12,19 @@ def is_verified(users):
         else:
             return True
     return commands.check(predicate)
+
+
+def is_staff(users):
+    async def predicate(ctx):
+        if users.find_one({"id": ctx.author.id}) is None:
+            raise user_checks("You do not have permission to use this command!")
+        else:
+            try:
+                permissions = users.find_one({"id": ctx.author.id})["Permissions"]
+                if permissions["isStaff"] is True:
+                    return True
+                else:
+                    raise user_checks("You do not have permission to use this command!")
+            except:
+                raise user_checks("You do not have permission to use this command!")
+    return commands.check(predicate)
