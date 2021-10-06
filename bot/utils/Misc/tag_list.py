4a6156d0586df7efde127e90871ae726966f2a13
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-10-06 08:26:39 -0400

    Create tags system with 2 tags

---
+++ bot/utils/Misc/tag_list.py
@@ -0,0 +1,66 @@
+import discord
+
+tag_list = {
+
+    "sykese info": {
+        "embedTitle": "Sykese FAQ",
+        "embedDescription": "There are a lot of questions that get asked regarding Sykese, so here's a short list "
+        "addressing the most common ones.",
+        "embedColor": discord.Color.blue(),
+        "fields": {
+            1: {
+                "name": "What is his username?",
+                "value": "sykeseguy",
+                "inline": False
+            },
+            2: {
+                "name": "What is his favorite color?",
+                "value": "Is that really a question you need to ask?",
+                "inline": False
+            },
+
+            3: {
+                "name": "Why don't you use your \"Sykese\" Minecraft account?",
+                "value": "He lost access to it years ago.",
+                "inline": False
+            },
+        }
+    },
+
+    "verify help": {
+        "embedTitle": "Verification Help",
+        "embedDescription": "It seems people tend to have certain difficulties when verifying. If this tag was called, "
+                            "your issue will more than likely be resolved by following the instructions below.",
+        "embedColor": discord.Color.blue(),
+        "fields": {
+            1: {
+                "name": "Command Usage",
+                "value": "+verify <Minecraft Username>. Yes, your __Minecraft__ username, not your Discord username. "
+                         "We already have access to that.",
+                "inline": False
+            },
+            2: {
+                "name": "How do I verify?",
+                "value": "Link your account to Hypixel, then run the command above. There is a gif pinned in the "
+                         "verification channel demonstrating how to do this.",
+                "inline": False
+            },
+            3: {
+                "name": "My current account doesn't match my linked account? What does that mean?",
+                "value": "Your linked account does not match with the account you are currently using. To resolve this,"
+                " you either need to update your linked account on Hypixel or change your Discord account to match the "
+                "linked one. Note that your account does not need to match after the verification process is over.",
+                "inline": False
+            },
+
+            4: {
+                "name": "I can't access Hypixel due to my account being banned. What do I do?",
+                "value": "We do not have an automatic verification system for users who cannot log into Hypixel. "
+                "Rather, you may follow the instructions at https://namemc.com/claim-your-profile, then link your "
+                "Discord account to your NameMC profile. After doing this, please ping a staff member in the "
+                "verification channel with a link to your NameMC account.",
+                "inline": False
+            },
+        }
+    }
+}
