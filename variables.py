5d83d1501da37e52710da89c8f22a4a52de92960
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-11-22 12:45:57 -0500

    Add guild lists

--- variables.py
+++ variables.py
@@ -1,3 +1,12 @@
 verified_role_id = 893933214656233563
 unverified_role_id = 893933243768918016
 mod_role_id = 891123241765179472
+
+guilds = [
+    889697074491293736,  # Sykese
+    707963219536248982  # test server
+    ]
+
+test_guilds = [
+    707963219536248982  # test server
+]
