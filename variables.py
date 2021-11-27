1bc4790c7ba1d75455e7903d9173ee9c1002a1b6
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-11-27 12:49:33 -0500

    set permission tiers

--- variables.py
+++ variables.py
@@ -10,3 +10,11 @@ guilds = [
 test_guilds = [
     707963219536248982  # test server
 ]
+
+permission_tiers = {
+    890700948710772766: 1,  # super friend
+    892475435290607646: 2,  # blue booster
+    911038229862563911: 3,  # birthday
+    891123241765179472: 90,  # chat moderator
+    889717645300072529: 100,  # admin
+}
