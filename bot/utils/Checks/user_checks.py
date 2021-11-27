814f0d671617b46f5a5da68c6c3f2b8199c314cc
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-11-27 12:49:52 -0500

    create better group_check() function

--- bot/utils/Checks/user_checks.py
+++ bot/utils/Checks/user_checks.py
@@ -1,4 +1,6 @@
 from discord.ext import commands
+from variables import permission_tiers
+import discord
 
 
 class user_checks(commands.CommandError):
@@ -32,3 +34,24 @@ def is_staff(users, permission_level=None):
             except:
                 raise user_checks("You do not have permission to use this command!")
     return commands.check(predicate)
+
+
+def group_check(author: discord.Member, minimum_permission_level: int):
+    role_list = []
+    if not len(author.roles):
+        return False
+
+    for role in author.roles:
+        role_list.append(role.id)
+
+    highest_role_permission = 0
+
+    for role_id in role_list:
+        if permission_tiers.get(role_id, None) is not None:
+            if permission_tiers[role_id] > highest_role_permission:
+                highest_role_permission = permission_tiers[role_id]
+
+    if highest_role_permission >= minimum_permission_level:
+        return True
+    else:
+        return False
