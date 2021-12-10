b88b393864e2bfb68abf1ecd1b05ca36d52b7a7b
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-12-10 10:39:53 -0500

    Adjust string formatting

--- bot/cogs/Verification/verify.py
+++ bot/cogs/Verification/verify.py
@@ -32,8 +32,8 @@ class verify_commands(commands.Cog):
             try:
                 discord_account = player["player"]["socialMedia"]["links"]["DISCORD"]
             except:
-                await ctx.respond("You need to link your Discord account to Hypixel! If you're unsure of how to do this, "
-                               "please view the gif below.\nhttps://imgur.com/2ZRQzEC.gif")
+                await ctx.respond("You need to link your Discord account to Hypixel! If you're unsure of how to do "
+                                  "this, please view the gif below.\nhttps://imgur.com/2ZRQzEC.gif")
                 return
 
             if str(ctx.author) != discord_account:
@@ -89,21 +89,28 @@ class verify_commands(commands.Cog):
                     return
             else:
                 embed = discord.Embed(title="Welcome to Sykese's Discord Server!",
-                                      description="Make sure to read the below information to continue into the server.",
+                                      description="Make sure to read the below information to continue into the "
+                                                  "server.",
                                       color=discord.Color.blue())
+
                 embed.add_field(name="Verification",
                                 value="We use a Hypixel based verification system. To verify, please "
-                                      "use the command `/verify <minecraft username>`. If you're unsure of how to link your "
-                                      "account, please view the gif pinned in <#893934059804319775>.", inline=False)
+                                      "use the command `/verify <minecraft username>`. If you're unsure of how to link "
+                                      "your account, please view the gif pinned in <#893934059804319775>.",
+                                inline=False)
+
                 embed.add_field(name="Rules & Information",
                                 value="Our server rules can be found in <#892118771257458738>. "
-                                      "By joining our server, you agree to follow these rules. You can also find important "
-                                      "information regarding the server and roles in this channel.", inline=False)
+                                      "By joining our server, you agree to follow these rules. You can also find "
+                                      "important information regarding the server and roles in this channel.",
+                                inline=False)
+
                 embed.add_field(name="Notification Roles", value="Once you verify, you'll get access to "
                                 "<#892208575273922581>. In this channel, you can choose selfroles that allow you to "
                                 "be notified for certain events, such as when Sykese uploads a video to his channel. "
                                 "These are completely optional, however we recommend that you enable them.",
                                 inline=False)
+
                 embed.set_footer(icon_url=member.guild.icon.url, text="Hope to see you around!")
                 try:
                     await member.send(embed=embed)
