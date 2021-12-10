0937a4dc12a587477615296a6ddcd31979b22b92
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-12-10 10:53:39 -0500

    Switch channel_restricted() decorator to new permission system

--- bot/utils/Checks/channel_checks.py
+++ bot/utils/Checks/channel_checks.py
@@ -1,7 +1,7 @@
+from bot.utils.Checks.user_checks import check_perms
 from discord.ext import commands
 
-whitelisted_channel_ids = [893934059804319775, 833711606470148206, 889697149279956993, 892116017977905242]
-
+whitelisted_channel_ids = [893934059804319775, 889697149279956993, 892116017977905242]
 
 class channel_checks(commands.CommandError):
     pass
@@ -9,21 +9,16 @@ class channel_checks(commands.CommandError):
 
 def channel_restricted():
     async def predicate(ctx):
-        if ctx.guild.id is not 889697074491293736:
+        if ctx.guild.id == 889697074491293736:
             return True
         if ctx.channel.id not in whitelisted_channel_ids:
-            try:
-                # Commenting out while this is reworked
-                #if group_check(ctx.author, 90):
-                #    return True
-                if 5 < 4:
-                    return True
-                else:
-                    await ctx.respond("You cannot use this command in this channel!", ephemeral=True)
-                    return
-            except:
-                await ctx.respond("You cannot use this command in this channel!", ephemeral=True)
-                return
+            if check_perms(ctx.author, ["bypass.channelRestrictions"]) is False:
+                error = "You cannot use this command in this channel!"
+                try:
+                    await ctx.respond(error, ephemeral=True)
+                except:
+                    await ctx.message.delete()
+                return False
         else:
             return True
     return commands.check(predicate)
