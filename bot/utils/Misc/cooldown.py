3d79d2f6b6d11861678ad61a6606163c9369d26e
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-12-27 12:45:02 -0500

    Create cooldown system that supports slash commands

---
+++ bot/utils/Misc/cooldown.py
@@ -0,0 +1,38 @@
+from variables import mod_id
+from discord.ext import commands
+import time
+import discord
+
+command_cache = {}
+
+
+def get_author_role_ids(author: discord.Member):
+    role_ids = []
+    if not len(author.roles):
+        return role_ids
+    else:
+        for role in author.roles:
+            role_ids.append(role.id)
+        return role_ids
+
+
+def cooldown(seconds: int, mod_bypass: bool = True):
+    async def predicate(ctx):
+        if mod_bypass is True and mod_id in get_author_role_ids(ctx.author):
+            return True
+        else:
+            try:
+                last_used = command_cache[ctx.author.id][ctx.command.name]["timestamp"]
+                if time.time() - last_used >= seconds:
+                    command_cache[ctx.author.id][ctx.command.name]["timestamp"] = time.time()
+                    return True
+                else:
+                    await ctx.respond(f"You can only use this command every {seconds} seconds. "
+                              f"({round(seconds - (time.time() - last_used))} seconds remaining)")
+                    return False
+            except:
+                command_cache[ctx.author.id][ctx.command.name] = {
+                    "timestamp": time.time()
+                }
+                return True
+    return commands.check(predicate)
