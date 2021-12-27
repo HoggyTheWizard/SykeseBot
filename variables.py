ff90351af1a38dc09a6d54f286dc7e06ae24eeab
HoggyTheWizard <hogwartswizrd@icloud.com>
2021-12-27 12:44:00 -0500

    mod_role_id -> mod_id

--- variables.py
+++ variables.py
@@ -1,6 +1,6 @@
 verified_role_id = 893933214656233563
 unverified_role_id = 893933243768918016
-mod_role_id = 891123241765179472
+mod_id = 891123241765179472
 
 guilds = [
     889697074491293736,  # Sykese
