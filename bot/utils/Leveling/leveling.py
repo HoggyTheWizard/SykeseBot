399d5e4c32053f3c5ee822f01c1b8bbeb9896a22
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-01-18 08:10:12 -0500

    Add support for level roles (role ids need to be added)

--- bot/utils/Leveling/leveling.py
+++ bot/utils/Leveling/leveling.py
@@ -4,6 +4,12 @@ exp_for_next_level = 100
 exp_for_next_prestige = exp_for_next_level * 25
 emoji_id = 678312701205807105
 
+# add role ids
+levelup_actions = {
+    "10": 0, "25": 0, "50": 0,
+    "75": 0, "100": 0, "125": 0,
+    "150": 0, "175": 0, "200": 0,
+}
 
 class LevelingMain(commands.CommandError):
     pass
@@ -28,10 +34,8 @@ class LevelingMain(commands.CommandError):
         levelup_xp_needed = 5 * (lvl ** 2) + (50 * lvl) + 100
         if exp >= levelup_xp_needed:
             users.update_one({"id": collection["id"]}, {"$set": {"Leveling.level": lvl + 1}})
-            emoji = self.bot.get_emoji(891388066634285096)
-            if lvl == 1:
-                await message.add_reaction(str(emoji))
-            elif lvl % 5 == 0:
-                await message.add_reaction(str(emoji))
-            elif lvl % 10 == 0:
-                await message.channel.send(f"Congrats <@{message.author.id}>, you're now level **{lvl}**!")
+            if str(lvl) in levelup_actions:
+                role = await message.guild.get_role(levelup_actions[str(lvl)])
+                await message.author.add_roles(role)
+                await message.channel.send(f"Congrats <@{message.author.id}>, you're now level **{lvl}** and "
+                                           f"have received the {role.name} role!")
