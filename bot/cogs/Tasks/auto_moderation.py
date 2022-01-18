3350cd0c8ff22358444d8f08d9ac6d7cb0649b39
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-01-18 08:09:11 -0500

    Check gif permissions based on role permissions

--- bot/cogs/Tasks/auto_moderation.py
+++ bot/cogs/Tasks/auto_moderation.py
@@ -1,30 +1,31 @@
 from discord.ext import commands
-from bot.utils.Checks.user_checks import check_perms
+from bot.utils.Checks.user_checks import check_perms, embed_perm
 from main import main_db
 from config import host
 import discord
 
 users = main_db["users"]
-blacklisted_domains = ["https://tenor.com/", "tenor.com/", "https://discord.gg/", "discord.gg/"]
+gif_domains = ["https://tenor.com/", "tenor.com"]
+blacklisted_domains = ["https://discord.gg/", "discord.gg/"]
 
 
 class auto_moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # Create exception for higher roled people posting tenor links
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.type == discord.MessageType.application_command:
             return
         elif message.guild.id == 889697074491293736 and host == "master":
-
+            if any(x in message.content for x in gif_domains):
+                if embed_perm(message.author) is False:
+                    await message.delete()
             if any(x in message.content for x in blacklisted_domains):
                 if check_perms(message.author, ["bypass.blacklistedDomains"]) is True:
                     return
                 else:
                     await message.delete()
-
 
 def setup(bot):
     bot.add_cog(auto_moderation(bot))
