578483e9d628a741437d679374fefcf5afbb15bc
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-01-18 08:28:00 -0500

    Remove unneeded imports

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from discord import ActivityType, AllowedMentions
 from BotLoader import BotLoader
 from discord.ext import commands
 from config import database_user, database_password, main_db_name
@@ -6,7 +5,7 @@ import logging
 import discord
 import config
 import pymongo
-import ssl
+
 cluster = pymongo.MongoClient(f"mongodb+srv://{database_user}:{database_password}@cluster0.2uexc.mongodb.net/"
                               f"{main_db_name}?retryWrites=true&w=majority")
 main_db = cluster["main_data"]
