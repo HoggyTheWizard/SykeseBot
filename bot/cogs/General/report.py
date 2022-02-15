cd16b015477663f9ee5df563f4f704018fa53ff3
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-02-14 21:27:17 -0500

    Continue working on report system

--- bot/cogs/General/report.py
+++ bot/cogs/General/report.py
@@ -1,18 +1,57 @@
-from discord.commands import slash_command as slash, user_command
+from discord.commands import user_command, Option, SlashCommandGroup
 from bot.utils.UI.report_modal import ReportModal
+from bot.utils.Checks.user import mod
+from db import main_db
 from discord.ext import commands
-import variables as v
+from bot import variables as v
 import discord
+
+users = main_db["users"]
+reports = main_db["reports"]
+archived_reports = main_db["archived_reports"]
+
 
 class Report(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    #@slash(guild_ids=v.guilds)
-    @user_command(guild_ids=v.guilds)
-    async def rep(self, ctx, member):
-        modal = ReportModal(title=f"Report Against {str(member)}", ctx=ctx)
+    report = SlashCommandGroup("report", "Report a user to the moderators.", guild_ids=v.guilds)
+
+    @user_command(name="Report", guild_ids=v.guilds)
+    async def report_user(self, ctx, member):
+        modal = ReportModal(title=f"Report Against {str(member)}", ctx=ctx, member=member)
         await ctx.interaction.response.send_modal(modal)
+
+    @report.command(guild_ids=v.guilds, description="Report a user to the moderators.")
+    async def create(self, ctx, member: Option(discord.Member, "The member you want to report")):
+        modal = ReportModal(title=f"Report Against {str(member)}", ctx=ctx, member=member)
+        await ctx.interaction.response.send_modal(modal)
+
+    @report.command(description="Staff Only - Handle a Report")
+    @mod()
+    async def handle(self, ctx, report_id: int,
+                     status: Option(str, "The status of the report", choices=["Accepted", "Not Enough Evidence",
+                                                                              "Denied"]),
+                     reason: Option(str, "The reason for the response")
+                     ):
+        report = reports.find_one({"id": report_id})
+        if report_id is None:
+            await ctx.respond("That report doesn't exist.")
+        else:
+            reporter = users.find_one({"id": report["reporter"]})
+            data = {
+                "status": status,
+                "activeReports": reporter.get("activeReports", 1) - 1
+            }
+            if status == "Accepted":
+                data["totalAcceptedReports"] = reporter.get("totalAcceptedReports", 0) + 1
+            elif status == "Not Enough Evidence":
+                data["totalNotEnoughEvidenceReports"] = reporter.get("totalNotEnoughEvidenceReports", 0) + 1
+            elif status == "Denied":
+                data["totalDeniedReports"] = reporter.get("totalDeniedReports", 0) + 1
+
+            users.update_one({"id": report["reporter"]}, {"$set": {data}})
+
 
 def setup(bot):
     bot.add_cog(Report(bot))
--- bot/utils/UI/report_modal.py
+++ bot/utils/UI/report_modal.py
@@ -1,11 +1,21 @@
 from discord.ui import InputText, Modal
+from db import main_db
+from discord.ext import commands
 import discord
+
+users = main_db["users"]
+
+
+class ReportHandler(commands.CommandError):
+    pass
 
 
 class ReportModal(Modal):
-    def __init__(self, ctx, *args, **kwargs) -> None:
+    def __init__(self, ctx, member, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        print(ctx.author.id)
+        self.ctx = ctx
+        self.member = member
+
         self.add_item(
             InputText(
                 label="Report Reason",
@@ -17,15 +27,29 @@ class ReportModal(Modal):
                 label=f"Detailed Information (Optional)",
                 placeholder="Go into detail about what this user has done that goes against our rules.",
                 style=discord.InputTextStyle.long,
-                max_length=1500,
+                max_length=1024,
                 required=False
             )
         )
 
     async def callback(self, interaction: discord.Interaction):
-        embed = discord.Embed(title="Your Modal Results", color=discord.Color.random())
-        embed.add_field(name="Submitted By:", value=f"{interaction.user.id}")
-        embed.add_field(name="Submitted By", value=self.children[0].value, inline=False)
-        embed.add_field(name="Second Input", value=self.children[1].value, inline=False)
-        await interaction.response.send_message(embeds=[embed])
+        user = users.find_one({"id": self.ctx.author.id})
+        if self.ctx.author.id == self.member.id:
+            raise ReportHandler("You can't report yourself.")
+
+        elif 891123241765179472 in [role.id for role in self.ctx.author.roles]:
+            raise ReportHandler("You can't report a staff member.")
+
+        elif user is None or user.get("activeReports", 0) >= 3:
+            raise ReportHandler("You can't have more than 3 active reports open at once. Please be patient.")
+
+        else:
+
+            embed = discord.Embed(title="New Report", color=discord.Color.red())
+            embed.add_field(name="Submitted By:", value=f"{interaction.user.mention}")
+            embed.add_field(name="Reported User:", value=self.member.mention)
+            embed.add_field(name="Report Tile:", value=self.children[0].value, inline=False)
+            embed.add_field(name="Report Details", value=self.children[1].value or "No Details Provided.", inline=False)
+            await self.ctx.guild.get_channel(942152380600950824).send(embed=embed)
+            await self.ctx.respond("Report submitted!", delete_after=3)
 
