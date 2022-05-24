784b169d859874a8d1be2b2e25e59f153bb4eb71
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-05-24 16:54:39 -0400

    Update profile command to include leveling information

--- bot/cogs/users/profile.py
+++ bot/cogs/users/profile.py
@@ -1,7 +1,14 @@
 from discord.commands import slash_command as slash, Option
+from bot.utils.Leveling.leveling import get_leveling
 from discord.ext import commands
+from datetime import datetime
+from db import main_db
 import bot.variables as v
 import discord
+
+users = main_db["users"]
+# To prevent unnecessary API calls, the bot will only update a skin every hour
+cached_skins = {}
 
 
 class Profile(commands.Cog):
@@ -9,9 +16,39 @@ class Profile(commands.Cog):
         self.bot = bot
 
     @slash(description="View the server profile of you or another member.", guild_ids=v.guilds)
-    async def profile(self, ctx, member: Option(discord.Member, "The member you want to view the profile of",
+    async def profile(self, ctx, member: Option(discord.Member, "The member you want to view the profile of.",
                                                 required=False)):
-        pass
+
+        member = ctx.author if member else member
+        collection = users.find_one({"id": member.id})
+
+        if not collection or not member:
+            await ctx.respond("Couldn't find any data for this member.", ephemeral=True)
+            return
+
+        name = member.nick if member.nick else member.name
+        leveling = get_leveling(collection)
+        cached_member = cached_skins.get(member.id)
+
+        if cached_member:
+            if cached_member.get("timestamp") + 3600 >= int(datetime.now().timestamp()):
+                skin = f"https://crafatar.com/renders/head/{collection['uuid']}?overlay=true"
+                cached_skins[member.id]["skin"] = skin
+                cached_skins[member.id]["timestamp"] = int(datetime.now().timestamp())
+            else:
+                skin = cached_skins[member.id]["skin"]
+        else:
+            skin = f"https://crafatar.com/renders/head/{collection['uuid']}?overlay=true"
+
+        embed = discord.Embed(title=f"{str(name)}'s Profile", color=member.color)
+        embed.set_thumbnail(url=skin)
+        embed.add_field(name="Linked Account:", value=f"{name} ({collection['uuid']})", inline=False)
+        embed.add_field(name="Level:", value=leveling["level"], inline=False)
+        embed.add_field(name="Experience:", value=leveling["exp"], inline=False)
+        embed.add_field(name="Needed EXP:", value=leveling["xp_needed"], inline=False)
+        if leveling.get("legacyLevel"):
+            embed.add_field(name="Legacy Level:", value=leveling["legacyLevel"], inline=False)
+        await ctx.respond(embed=embed)
 
 
 def setup(bot):
