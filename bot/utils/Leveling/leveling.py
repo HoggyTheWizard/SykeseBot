58c664f364c4cd804469626e177b09d130e6d335
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-05-24 16:55:09 -0400

    Clean up code

--- bot/utils/Leveling/leveling.py
+++ bot/utils/Leveling/leveling.py
@@ -1,5 +1,3 @@
-from discord.ext import commands
-
 levelup_actions = {
     "15": 933130048192544838, "25": 933130059731066930, "50": 933130064927784970,
     "75": 933130067284992050, "100": 933130061949849691, "125": 933130057063481384,
@@ -7,41 +5,34 @@ levelup_actions = {
 }
 
 
-class LevelingMain(commands.CommandError):
-    pass
+def get_leveling(collection):
+    obj = collection.get("Leveling", None)
+    payload = obj if obj else {"exp": 100, "level": 1}
+    xp_needed = 5 * obj["level"] ** 2 + 50 * obj["level"] + 100 - obj["exp"]
+    payload["xp_needed"] = xp_needed
+    payload["lastTriggeredMessage"] = payload["lastTriggeredMessage"] if "lastTriggeredMessage" in payload else None
+    return payload
 
-    @staticmethod
-    def get_leveling(collection, users):
-        if collection is None:
-            return None
-        else:
-            leveling_object = collection.get("Leveling", None)
-            if leveling_object is not None:
-                return leveling_object
-            elif leveling_object is None:
-                users.update_one({"id": collection["id"]}, {"$set": {"Leveling": {"exp": 0}}})
-                return users.find_one({"id": collection["id"]})["Leveling"]
 
-    @staticmethod
-    async def levelup(guild, users, collection, leveling, member, message=None):
-        exp = leveling.get("exp", 0)
-        lvl = leveling.get("level", 0)
-        # Taken from mee6 (the only good thing they've ever done)
-        levelup_xp_needed = 5 * (lvl ** 2) + (50 * lvl) + 100
-        if exp >= levelup_xp_needed:
-            users.update_one({"id": collection["id"]}, {"$set": {"Leveling.level": lvl + 1}})
-            if str(lvl+1) in levelup_actions:
-                added_role = guild.get_role(levelup_actions[str(lvl+1)])
-                if message is None:
-                    channel = guild.get_channel(889697074491293740)
-                else:
-                    channel = message.channel
+async def levelup(guild, users, collection, leveling, member, message=None):
+    exp = leveling["exp"]
+    lvl = leveling["level"]
+    # Taken from mee6 (the only good thing they've ever done)
+    levelup_xp_needed = 5 * lvl ** 2 + 50 * lvl + 100 - exp
+    if exp >= levelup_xp_needed:
+        users.update_one({"id": collection["id"]}, {"$inc": {"Leveling.level": 1}})
+        if str(lvl+1) in levelup_actions:
+            added_role = guild.get_role(levelup_actions[str(lvl+1)])
+            if message is None:
+                channel = guild.get_channel(889697074491293740)
+            else:
+                channel = message.channel
 
-                # removing all roles that a user has that don't correspond with their level
-                same_roles = [x for x in [role.id for role in member.roles]
-                              if x in [levelup_actions[x] for x in levelup_actions]]
-                for role in same_roles:
-                    await member.remove_roles(guild.get_role(role))
-                await member.add_roles(added_role)
-                await channel.send(f"Congrats <@{member.id}>, you're now level **{lvl+1}** and "
-                                   f"have received the **{added_role.name}** role!")
+            # removing all roles that a user has that don't correspond with their level
+            same_roles = [x for x in [role.id for role in member.roles]
+                          if x in [levelup_actions[x] for x in levelup_actions]]
+            for role in same_roles:
+                await member.remove_roles(guild.get_role(role))
+            await member.add_roles(added_role)
+            await channel.send(f"Congrats <@{member.id}>, you're now level **{lvl+1}** and "
+                               f"have received the **{added_role.name}** role!")
