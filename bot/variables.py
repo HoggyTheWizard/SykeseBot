180ad6c8cc267bc4cb4c07a818ee3c9f0d1dbd4a
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-05-24 17:36:46 -0400

    Add test_manager_id

--- bot/variables.py
+++ bot/variables.py
@@ -4,6 +4,7 @@ verified_role_id = 893933214656233563
 unverified_role_id = 893933243768918016
 sync_lock_id = 977736559921070130
 mod_id = 891123241765179472
+test_manager_id = 710922532131307531
 
 owner_ids = [524755715655467019, 192097613087113220]
 
@@ -23,6 +24,6 @@ else:
 mod_id = 891123241765179472
 developer_id = 892811975329980447
 
-moderator_ids = [mod_id, developer_id]
+moderator_ids = [mod_id, developer_id, test_manager_id]
 manager_ids = [developer_id]
 bot_logs = 977734834929672302
