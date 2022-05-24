d3b64cee8eba47b27f30d6f3dd3b08594fa1997b
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-05-24 09:11:23 -0400

    Create a separate embed for post-verification users and change class naming convention

--- bot/cogs/Verification/verify.py
+++ bot/cogs/Verification/verify.py
@@ -2,52 +2,86 @@ from discord.ext import commands
 from discord.commands import slash_command as slash
 from bot.variables import guilds
 from db import main_db
-from bot.utils.Misc.general import aiohttp_json, get_mojang_from_username
-from bot.utils.Checks.channel_checks import channel_restricted
+from bot.utils.Misc.requests import player
 from bot.variables import verified_role_id, unverified_role_id
 from datetime import datetime
 import discord
 import config
 
-
 users = main_db["users"]
 
 
-class verify_commands(commands.Cog):
+class Verify(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @slash(description="Links a Minecraft account to your Discord account.", guild_ids=guilds)
-    @commands.guild_only()
-    @channel_restricted()
     async def verify(self, ctx, username):
         if users.find_one({"id": ctx.author.id}):
             await ctx.respond("You're already verified to the account "
-                           f"`{users.find_one({'id' : ctx.author.id}).get('uuid', 'ERROR')}`")
+                              f"`{users.find_one({'id': ctx.author.id}).get('uuid', 'ERROR')}`")
         else:
-            mojang = await get_mojang_from_username(username=username)
-            player = await aiohttp_json(endpoint="player", attribute=mojang["id"])
+
+            p = await player(name=username)
+
+            if not p:
+                await ctx.respond("Couldn't find any data regarding that account. There are two reasons why this error "
+                                  "may have been triggered:\n1. The Minecraft username you entered may be invalid. Are "
+                                  f"you sure `{username}` is your current __Minecraft__ username?\n2."
+                                  f" The account you have chosen has never logged on to Hypixel.")
+                return
 
             try:
-                discord_account = player["player"]["socialMedia"]["links"]["DISCORD"]
-            except:
+                discord_account = p["player"]["socialMedia"]["links"]["DISCORD"]
+            except KeyError:
                 await ctx.respond("You need to link your Discord account to Hypixel! If you're unsure of how to do "
                                   "this, please view the gif below.\nhttps://imgur.com/2ZRQzEC.gif")
                 return
 
             if str(ctx.author) != discord_account:
-                if "discord.gg/" in discord_account:
-                    account = "Blocked linked account due to it being an invite link."
-                else:
-                    account = discord_account
+                account = discord_account if "discord.gg" not in discord_account else \
+                    "Blocked linked account due to it being an invite link."
                 await ctx.respond(f"Your accounts don't match!\n\nYour Account: {str(ctx.author)}\n"
-                               f"Linked Account: {account}\n\nYou either need to change your current account to match "
-                               "your linked account or update your linked account.")
+                                  f"Linked Account: {account}\n\nYou either need to change your current account to "
+                                  f"match your linked account or update your linked account.")
 
-            elif str(ctx.author) == player["player"]["socialMedia"]["links"]["DISCORD"]:
+            elif str(ctx.author) == p["player"]["socialMedia"]["links"]["DISCORD"]:
                 await ctx.author.add_roles(ctx.guild.get_role(verified_role_id))
-                users.insert_one({"id": ctx.author.id, "uuid": mojang["id"],
+                users.insert_one({"id": ctx.author.id, "uuid": p["player"]["uuid"],
                                   "verifiedAt": datetime.timestamp(datetime.now())})
+
+                embed = discord.Embed(title="Verification Successful!",
+                                      description=f"Thank you for verifying your account in the {ctx.guild.name} "
+                                                  f"server! Below is some helpful information to help you get started "
+                                                  f"in our server.",
+                                      color=discord.Color.blue())
+                embed.add_field(name="Rules & Information",
+                                value="Our server rules can be found in <#892118771257458738>. "
+                                      "By joining our server, you agree to follow these rules. If you see"
+                                      "a member breaking our rules, please use the `/report` command so that it can"
+                                      "be handled by our moderation team.",
+                                inline=False)
+
+                embed.add_field(name="Notification Roles",
+                                value="To assign roles to yourself, head over to <#892208575273922581>. This will "
+                                      "allow you be notified of certain events, such as when Sykese uploads a video "
+                                      "to his channel or starts streaming. These are optional, however we recommend "
+                                      "that you enable them.",
+                                inline=False)
+
+                embed.add_field(name="Suggestions and Feedback",
+                                value="Have any suggestions regarding our server or Sykese's channel? Head over to our "
+                                      "Suggestions and Feedback category to voice your opinions. Make sure to keep "
+                                      "your opinions constructive; all feedback is welcome if voiced in a respectful "
+                                      "and constructive manner.",
+                                inline=False)
+
+                embed.set_footer(icon_url=ctx.guild.icon.url, text="Have a good day!")
+                try:
+                    await ctx.author.send(embed=embed)
+                except:
+                    return
+
                 await ctx.respond("Successfully verified!")
 
                 try:
@@ -59,7 +93,7 @@ class verify_commands(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        if member.guild.id in [889697074491293736] and config.host == "master":
+        if member.guild.id in guilds and config.host == "master":
             if users.find_one({"id": member.id}) is not None:
                 await member.add_roles(member.guild.get_role(verified_role_id))
                 try:
@@ -75,20 +109,9 @@ class verify_commands(commands.Cog):
 
                 embed.add_field(name="Verification",
                                 value="We use a Hypixel based verification system. To verify, please "
-                                      "use the command `/verify <minecraft username>`. If you're unsure of how to link "
-                                      "your account, please view the gif pinned in <#893934059804319775>.",
-                                inline=False)
-
-                embed.add_field(name="Rules & Information",
-                                value="Our server rules can be found in <#892118771257458738>. "
-                                      "By joining our server, you agree to follow these rules. You can also find "
-                                      "important information regarding the server and roles in this channel.",
-                                inline=False)
-
-                embed.add_field(name="Notification Roles", value="Once you verify, you'll get access to "
-                                "<#892208575273922581>. In this channel, you can choose selfroles that allow you to "
-                                "be notified for certain events, such as when Sykese uploads a video to his channel. "
-                                "These are completely optional, however we recommend that you enable them.",
+                                      "use the command `/verify <minecraft username>` in the <#893934059804319775> "
+                                      "channel. If you're unsure of how to link your account, please view the pinned "
+                                      "gif",
                                 inline=False)
 
                 embed.set_footer(icon_url=member.guild.icon.url, text="Hope to see you around!")
@@ -99,4 +122,4 @@ class verify_commands(commands.Cog):
 
 
 def setup(bot):
-    bot.add_cog(verify_commands(bot))
+    bot.add_cog(Verify(bot))
