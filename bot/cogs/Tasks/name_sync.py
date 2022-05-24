a708891031af2629dfac11cabdf4f3f2d885fa15
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-05-24 08:08:38 -0400

    Create namesync task

---
+++ bot/cogs/Tasks/name_sync.py
@@ -0,0 +1,53 @@
+from discord.ext import commands, tasks
+from bot.utils.Misc.requests import mojang
+from db import main_db
+import bot.variables as v
+import discord
+
+users = main_db["users"]
+
+
+class NameSync(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.name_sync.start()
+
+    @tasks.loop(hours=1)
+    async def name_sync(self):
+        guild = self.bot.get_guild(v.guilds[0])
+        channel = guild.get_channel(v.bot_logs)
+        success = 0
+        exempt = 0
+        no_change = 0
+        failed = 0
+
+        for member in guild.members:
+            if member.bot or len([role.id for role in member.roles if role.id == v.sync_lock_id]):
+                exempt += 1
+                continue
+
+            doc = users.find_one({"id": member.id})
+            request = await mojang(doc.get("uuid", None))
+
+            if doc is None or request is None:
+                no_change += 1
+                continue
+
+            elif member.nick is None and request["name"] != member.name or member.nick != request["name"]:
+                try:
+                    await member.edit(nick=request["name"])
+                    success += 1
+                except discord.Forbidden:
+                    failed += 1
+                    await channel.send(f"I don't have permission to change {str(member)}'s nickname. ({member.id})")
+
+        await channel.send(f"Finished syncing names:\n{success} successful\n{failed} failed\n{exempt} "
+                           f"exempt\n{no_change} no change")
+
+    @name_sync.before_loop
+    async def before_name_sync(self):
+        await self.bot.wait_until_ready()
+
+
+def setup(bot):
+    bot.add_cog(NameSync(bot))
