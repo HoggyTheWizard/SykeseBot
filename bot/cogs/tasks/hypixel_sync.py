1e3e8bbd3f31b5089bfebd0f1a02b6f6570c8634
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-06-06 19:16:26 -0400

    Add support for removing old rank/level roles

--- bot/cogs/tasks/hypixel_sync.py
+++ bot/cogs/tasks/hypixel_sync.py
@@ -3,8 +3,6 @@ from bot.utils.hypixel.player import Player, levels, ranks
 from bot.utils.misc.requests import player
 from db import main_db
 import bot.variables as v
-import discord
-import aiohttp
 import config
 
 users = main_db["users"]
@@ -24,13 +22,13 @@ class HypixelSync(commands.Cog):
         level_roles = [x[1] for x in levels]
         guild = self.bot.get_guild(v.guilds[0])
         channel = guild.get_channel(v.bot_logs)
-        session = aiohttp.ClientSession()
         success = 0
         exempt = 0
         no_change = 0
         failed = 0
 
         for member in guild.members:
+            # creates a list of all roles the user has pertaining to Hypixel level and rank
             roles = [role.id for role in member.roles if role.id in [x["role"] for x in ranks] or
                      role.id in [x[1] for x in levels]]
             if member.bot or len([role for role in roles if role.id == v.sync_lock_id]) or \
@@ -39,7 +37,7 @@ class HypixelSync(commands.Cog):
                 continue
 
             doc = users.find_one({"id": member.id})
-            request = await player(session=session, uuid=doc.get("uuid", None))
+            request = await player(uuid=doc.get("uuid", None))
 
             if not doc or not request:
                 no_change += 1
@@ -50,18 +48,21 @@ class HypixelSync(commands.Cog):
             # rank
             rank = [x for x in ranks if any(x in roles for x in rank_roles)]
             if p.rank()["role"] != rank:
+                await member.remove_roles(guild.get_role(rank[0]))
                 await member.add_roles(guild.get_role(p.rank()["role"]))
 
             # leveling
             level = [x for x in levels if any(x in roles for x in level_roles)]
             if p.level()[1] != level:
+                await member.remove_roles(guild.get_role(level[0]))
                 await member.add_roles(guild.get_role(p.level()[1]))
 
         await channel.send(f"Finished syncing names:\n{success} successful\n{failed} failed\n{exempt} "
                            f"exempt\n{no_change} no change")
 
     @hypixel_sync.before_loop
-    async def before_name_sync(self):
+    async def before_hypixel_sync(self):
+        print("Prepping Hypixel sync...")
         await self.bot.wait_until_ready()
 
 
