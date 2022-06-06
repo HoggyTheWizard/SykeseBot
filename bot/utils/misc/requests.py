cf5d3c3cf3671f75e596aceefd2a0e408280bb3b
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-06-06 19:13:44 -0400

    Switch the way sessions are handled

--- bot/utils/misc/requests.py
+++ bot/utils/misc/requests.py
@@ -12,44 +12,39 @@ class RequestParamsUnfulfilled(core.ApplicationCommandError):
     pass
 
 
-async def mojang(uuid_or_name: str, session: aiohttp.ClientSession, counter: int = 0):
-    async with session.get(f"https://api.mojang.com/users/profiles/minecraft/{uuid_or_name}") as r:
-        # status 429 means the Mojang API ratelimit (600 per 10 min) has been reached
+async def mojang(uuid_or_name: str, counter: int = 0):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://api.mojang.com/users/profiles/minecraft/{uuid_or_name}") as r:
+            print("m session fetched", r.status)
+            # status 429 means the Mojang API ratelimit (600 per 10 min) has been reached
+            if r.status == 429:
 
-        if r.status == 429:
+                if counter > 5:
+                    raise ApiException(f"The Mojang API ratelimit has been reached and exceeded multiple times. "
+                                       f"Please check to make sure the ratelimit has not been changed.")
+                await asyncio.sleep(120)
+                await mojang(uuid_or_name, counter+1)
 
-            if counter > 5:
-                raise ApiException(f"The Mojang API ratelimit has been reached and exceeded multiple times. "
-                                   f"Please check to make sure the ratelimit has not been changed.")
-            await asyncio.sleep(120)
-            await mojang(uuid_or_name, session, counter+1)
-
-        elif r.status == 200:
-            return r.json()
-
-        else:
-            return None
+            return await r.json() if r.status == 200 else None
 
 
-async def player(session, uuid=None, name=None, counter=0):
+async def player(uuid=None, name=None, counter=0):
     if not name and not uuid:
         raise RequestParamsUnfulfilled("You must provide either a UUID or a username.")
+
     if not uuid and name:
-        m = await mojang(uuid_or_name=name, session=session)
-        uuid = m["id"]
-
+        m = await mojang(uuid_or_name=name)
+    else:
+        m = {"id": uuid}
     counter = counter
-    async with session.get(f"https://api.hypixel.net/player?key={key}&uuid={uuid}") as r:
-        # Hypixel API ratelimit reached (120 per minute)
-        if r.status == 429:
-            if counter > 2:
-                raise ApiException(f"The Hypixel API ratelimit has been reached and exceeded multiple times. "
-                                   f"Please check to make sure the ratelimit has not been changed.")
-            await asyncio.sleep(61)
-            await player(session=session, uuid=uuid, counter=counter+1)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(f"https://api.hypixel.net/player?key={key}&uuid={m['id']}") as r:
+            # Hypixel API ratelimit reached (120 per minute)
+            if r.status == 429:
+                if counter > 2:
+                    raise ApiException(f"The Hypixel API ratelimit has been reached and exceeded multiple times. "
+                                       f"Please check to make sure the ratelimit has not been changed.")
+                await asyncio.sleep(61)
+                await player(uuid=uuid, counter=counter+1)
 
-        if r.status == 200:
-            return r.json()
-
-        else:
-            return None
+            return await r.json() if r.status == 200 else None
