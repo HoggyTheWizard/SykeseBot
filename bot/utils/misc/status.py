9979da325500acbf4771c928eab51aef7a15d1be
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-07-31 14:52:37 -0400

    Create status switcher task

---
+++ bot/utils/misc/status.py
@@ -0,0 +1,21 @@
+from random import choice
+
+
+def get_status(guild, yt):
+    channel = yt.channels().list(
+        part="statistics",
+        id="UCB5dggJv599ioXUL11d0LXg"
+    ).execute()
+    statistics = channel["items"][0]["statistics"]
+    statuses = [
+        f"Thank you to the {statistics.get('subscriberCount', 'ERROR')} people subscribed to Sykese",
+        f"Sykese currently has {statistics.get('videoCount', 'ERROR')} videos",
+        f"{round(int(statistics.get('subscriberCount', 0))/30_000, 2)*100}% to 30,000 subscribers",
+        f"Thank you for boosting {choice(guild.get_role(892475435290607646).members).name}!"
+        "Blue is the best color",
+        f"Hello {choice(guild.members).name}, thanks for being in our server",
+        "Want to check your server level? Use the `/profile` command",
+        "Want to see where you rank amongst your fellow server members? Use the `/level_leaderboard` command",
+        "Follow Sykese on the forums to help him become the most followed user on the site"
+    ]
+    return choice(statuses)
