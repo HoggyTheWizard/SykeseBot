bb533dec797a1e35940a4602b1576d2af120a483
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-07-31 15:24:23 -0400

    name -> nickname

--- bot/utils/misc/status.py
+++ bot/utils/misc/status.py
@@ -11,9 +11,9 @@ def get_status(guild, yt):
         f"Thank you to the {statistics.get('subscriberCount', 'ERROR')} people subscribed to Sykese",
         f"Sykese currently has {statistics.get('videoCount', 'ERROR')} videos",
         f"{int((int(statistics.get('subscriberCount', 0))/30_000)*100)}% to 30,000 subscribers",
-        f"Thank you for boosting {choice(guild.get_role(892475435290607646).members).name}!",
+        f"Thank you for boosting {choice(guild.get_role(892475435290607646).members).nick}!",
         "Blue is the best color",
-        f"Hello {choice(guild.members).name}, thanks for being in our server",
+        f"Hello {choice(guild.get_role(893933214656233563).members).nick}, thanks for being in our server",
         "Want to check your server level? Use the `/profile` command",
         "Want to see where you rank amongst your fellow server members? Use the `/level_leaderboard` command",
         "Follow Sykese on the forums to help him become the most followed user on the site"
