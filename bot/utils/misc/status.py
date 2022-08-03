70449c719a8644628f95650953dfce23b3979088
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-08-03 16:37:05 -0400

    Fixed the name displaying as None if the member doesn't have a nickname

--- bot/utils/misc/status.py
+++ bot/utils/misc/status.py
@@ -7,13 +7,14 @@ def get_status(guild, yt):
         id="UCB5dggJv599ioXUL11d0LXg"
     ).execute()
     statistics = channel["items"][0]["statistics"]
+    name = choice(guild.get_role(893933214656233563).members).display_name
     statuses = [
         f"Thank you to the {statistics.get('subscriberCount', 'ERROR')} people subscribed to Sykese",
         f"Sykese currently has {statistics.get('videoCount', 'ERROR')} videos",
         f"{int((int(statistics.get('subscriberCount', 0))/30_000)*100)}% to 30,000 subscribers",
-        f"Thank you for boosting {choice(guild.get_role(892475435290607646).members).nick}!",
+        f"Thank you for boosting {name}!",
         "Blue is the best color",
-        f"Hello {choice(guild.get_role(893933214656233563).members).nick}, thanks for being in our server",
+        f"Hello {name}, thanks for being in our server",
         "Want to check your server level? Use the `/profile` command",
         "Want to see where you rank amongst your fellow server members? Use the `/level_leaderboard` command",
         "Follow Sykese on the forums to help him become the most followed user on the site"
