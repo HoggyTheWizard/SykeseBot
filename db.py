0b770c47835f773bc4539b999ba25fea938b8dbb
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-08-24 07:50:23 -0400

    Maybe this will fix the KeyError issue???

--- db.py
+++ db.py
@@ -1,5 +1,8 @@
+from dotenv import load_dotenv
 import pymongo
 import os
+
+load_dotenv("config.env")
 
 cluster = pymongo.MongoClient(f"mongodb+srv://{os.environ['DATABASE_USER']}:{os.environ['DATABASE_PASSWORD']}"
                               f"@cluster0.2uexc.mongodb.net/{os.environ['MAIN_DB_NAME']}?retryWrites=true&w=majority")
