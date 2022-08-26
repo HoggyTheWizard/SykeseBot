10191b48bb1b4e351edb9e584b51e35d75396954
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-08-26 14:33:43 -0400

    Move giveaway end process to a function

--- bot/cogs/tasks/end_giveaways.py
+++ bot/cogs/tasks/end_giveaways.py
@@ -24,30 +24,8 @@ class EndGiveaways(commands.Cog):
                 winners = pick_winner(guild, giveaway["participants"], winners=giveaway["totalWinners"])
                 if not len(winners):
                     await guild.get_channel(giveaway).send(f"No one entered the giveaway. Very sad. :(")
-                giveaways.update_one(
-                    {"id": giveaway["id"]}, {"$set": {"active": False, "winners": [winner.id for winner in winners]}}
-                )
-
-                try:
-                    message = await self.bot.get_channel(v.giveaways).fetch_message(giveaway["message"])
-                except:
-                    await guild.get_channel(v.bot_logs).send(f"Couldn't find a giveaway message for giveaway "
-                                                             f"`{giveaway['id']}`. If this is intentional, "
-                                                             f"ignore this message.")
-                    continue
-                embed = message.embeds[0]
-                embed.color = discord.Color.red()
-                embed.set_footer(text=f"{winners[0].display_name if len(winners) == 1 else f'{len(winners)} people'} "
-                                      f"won the giveaway!")
-                view = discord.ui.View.from_message(message)
-                view.disable_all_items()
-                await message.edit(embed=embed, view=view)
-                pings = ""
-                for winner in winners:
-                    pings += f"{winner.mention} "
-
-                await message.reply(f"Congratulations to {pings}for winning the giveaway! Please contact the host for "
-                                    f"more info.")
+                    return
+                await end_giveaway(self, giveaway)
 
     @end_giveaways.before_loop
     async def before_end_giveaways(self):
@@ -81,6 +59,38 @@ def pick_winner(guild: discord.Guild, participants: list, winners: int = 1):
             winners -= 1
 
     return winner_list
+
+
+async def end_giveaway(self, giveaway: dict):
+    guild = self.bot.get_guild(v.guilds[0])
+    participants = giveaway["participants"]
+    participants = [x for x in participants if x not in giveaway["winners"]]
+    winners = pick_winner(guild, participants, winners=giveaway["totalWinners"])
+    giveaways.update_one(
+        {"id": giveaway["id"]}, {"$set": {"active": False, "winners": [winner.id for winner in winners]}}
+    )
+
+    try:
+        message = await guild.get_channel(v.giveaways).fetch_message(giveaway["message"])
+    except:
+        await guild.get_channel(v.bot_logs).send(f"Couldn't find a giveaway message for giveaway "
+                                                 f"`{giveaway['id']}`. If this is intentional, "
+                                                 f"ignore this message.")
+        return
+
+    embed = message.embeds[0]
+    embed.color = discord.Color.red()
+    embed.set_footer(text=f"{winners[0].display_name if len(winners) == 1 else f'{len(winners)} people'} "
+                          f"won the giveaway!")
+    view = discord.ui.View.from_message(message)
+    view.disable_all_items()
+    await message.edit(embed=embed, view=view)
+    pings = ""
+    for winner in winners:
+        pings += f"{winner.mention} "
+
+    await message.reply(f"Congratulations to {pings}for winning the giveaway! Please contact the host for "
+                        f"more info.")
 
 
 def setup(bot):
