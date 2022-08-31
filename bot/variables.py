a6af2b97ade7d8c1842e837ea92b4df59b22f2ff
HoggyTheWizard <hogwartswizrd@icloud.com>
2022-08-31 09:14:22 -0400

    add mention_spam_rule and moderator_commands

--- bot/variables.py
+++ bot/variables.py
@@ -4,6 +4,7 @@ verified_role_id = 893933214656233563
 unverified_role_id = 893933243768918016
 sync_lock_id = 977736559921070130
 mod_id = 891123241765179472
+developer_id = 892811975329980447
 test_manager_id = 710922532131307531
 giveaway_role = 1012698422060515348
 blue_freaks_bling_id = 1006289544116719735
@@ -21,11 +22,10 @@ test_guilds = guilds = [
 
 guilds = main_guilds if config.host == "master" else test_guilds
 
-
-mod_id = 891123241765179472
-developer_id = 892811975329980447
-
 moderator_ids = [mod_id, developer_id, test_manager_id]
 manager_ids = [developer_id]
 bot_logs = 977734834929672302
 giveaways = 1012328814757543946
+moderator_commands = 889697149279956993
+
+mention_spam_rule = 1014509222119096371
